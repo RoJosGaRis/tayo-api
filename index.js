60eda92529b9963533a3449d62a039da1fd20aa1
RoJosGaRis <roberto.jgr03@gmail.com>
2024-03-09 18:34:04 -0600

    Add dinRouter to handle /dins endpoint

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
+const dinRouter = require("./routes/dins");
 
 const port = 3000;
+
+app.use("/dins", dinRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at PORT:${port}`);
 });
